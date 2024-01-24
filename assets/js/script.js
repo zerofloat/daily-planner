d14cbe2588a75c1d1a03992ab20009b73a78acd8
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-01-24 14:35:40 +0000

    add bootstrap cols and classes, FA icon +  add dayJS advancedFormat plugin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 console.log("JS loaded");
 
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+
 var timeNowEl = document.getElementById("currentDay")
 
-var calendarDate = dayjs().format('dddd DD MMMM');
+var calendarDate = dayjs().format('Do MMMM YYYY');
 
 timeNowEl.textContent = calendarDate;
-
-
-// timeNowEl.textcontent = 
 
 console.log(calendarDate);
